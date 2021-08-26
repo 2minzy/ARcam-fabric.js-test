b6411bf3d46c27912e3f186638ae8fdc0db06999
Minzy <minjilee0423@gmail.com>
2021-08-26 17:09:03 +0900

    🚀 Update routes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,30 @@
+const genPage = function(items) {
+	var pageInfo = {};
+	for(const item of items) {
+		pageInfo[item] = {
+			entry: "src/main.js",
+			template: "public/index.html",
+			filename: item + "/index.html"
+		}
+	}
+	return pageInfo;
+}
+
 module.exports = {
   publicPath: '/ARcam-fabric.js-test/',
-  outputDir: './docs'
+  outputDir: './docs',
+  pages: {
+		home: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Home',
+    },
+    ...genPage([
+			"captureFabric",
+			"captureFabricTest",
+      "facingMode",
+      "landing"
+		])
+  },
 }
